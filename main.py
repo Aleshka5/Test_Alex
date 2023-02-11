878b18414f570c169dd1793efafb07ee53c45cef
Aleshka5 <78702396+Aleshka5@users.noreply.github.com>
2023-02-11 21:26:34 +0300

    a small edit

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import html_parser
 import parser_requests
 
 def scrape_images(url):
-    # Free proxy configuration
-    proxy_host = '135.181.14.45'
-    proxy_port = '5959'
-
-    #parser_selenium.parse_the_cite(url,proxy_ip=proxy_host,proxy_port=proxy_port,use_new_proxi=False) # Gets the website source and places it in a folder named "templates"
+    parser_selenium.parse_the_cite(url) # Gets the website source and places it in a folder named "templates"
     photo_urls = html_parser.take_photo_urls() # Takes all needed links to images from HTML pages
     parser_requests.get_photos(photo_urls[0],photo_urls[1]) # Saves all images to a folder named "imgs"
 
