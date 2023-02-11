d282ea53eb6358bd66a4e76b09d824d0139c1f5e
Aleshka5 <78702396+Aleshka5@users.noreply.github.com>
2023-02-11 14:59:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import html_parser
 import parser_requests
 
 def scrape_images(url):
-    #parser_selenium.parse_the_cite(url)             # Получает код сайта в нескольких пакетах и сохраняет в текущую директорию
+    parser_selenium.parse_the_cite(url)             # Получает код сайта в нескольких пакетах и сохраняет в текущую директорию
     photo_urls = html_parser.take_photo_urls()     # Получает со страниц все ссылки на нужные фотографии
     parser_requests.get_photos(photo_urls[0],photo_urls[1])          # Сохраняет все фоотографии в папку imgs
 
